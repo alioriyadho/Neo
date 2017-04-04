dd6726f9559e6484bda12d74301616f77a5fd782
Ali Riyadh <alioriyadho@gmail.com>
2017-04-04 13:40:53 +0200

    Databasen är inte längre ignorerad från projektet.

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -61,9 +61,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(230, 45);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 33);
+            this.button1.Size = new System.Drawing.Size(176, 49);
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
