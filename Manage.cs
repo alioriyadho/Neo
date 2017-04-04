3ea0ca704704b805bfcabe5726335610c7d676e0
Ali Riyadh <alioriyadho@gmail.com>
2017-04-04 16:21:00 +0200

    Försök att koppla upp till db men har inte lyckats än. Priblem med connstring.

--- Manage.cs
+++ Manage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,29 @@ namespace Neo
         public Manage()
         {
             InitializeComponent();
+
+            // Fyll i listvyn
+            getDataFromDb();
+        }
+
+        public void getDataFromDb()
+        {
+            listView1.View = View.Details;
+
+            string constring = "Data Source=(LocalDB)/MSSQLLocalDB;AttachDbFilename=H:/Neonatalvården/Neo/Neo/db.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constring);
+            SqlDataAdapter ada = new SqlDataAdapter("select * from Children", con);
+            DataTable dt = new DataTable();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+                ListViewItem listitem = new ListViewItem(dr["person_id"].ToString());
+                listitem.SubItems.Add(dr["first_name"].ToString());
+                listitem.SubItems.Add(dr["first_name"].ToString());
+                listitem.SubItems.Add(dr["first_name"].ToString());
+                listView1.Items.Add(listitem);
+            }
         }
     }
 }
